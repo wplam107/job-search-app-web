772ed8ce12a18141f7551e61e8780dc09c1b10f7
Wayne Lam <w.p.lam107@gmail.com>
2022-07-07 18:36:11 -0400

    fix

--- components/ActiveLink.jsx
+++ components/ActiveLink.jsx
@@ -6,9 +6,20 @@ function ActiveLink({ children, href, name }) {
   const router = useRouter();
 
   useEffect(() => {
+    let token = '';
+    if (localStorage.getItem("supabase.auth.token") !== null || localStorage.getItem("supabase.auth.token") !== undefined) {
+      const cookie = JSON.parse(localStorage.getItem("supabase.auth.token"));
+      const currentSession = cookie["currentSession"];
+      try {
+        token = currentSession["access_token"];
+      } catch {
+        token = '';
+      }
+    }
+
     if (router.asPath === href) {
       setClassName('text-orange-400');
-    } else if (name === 'home' && router.asPath === '/#') {
+    } else if (name === 'home' && router.asPath === `/#${token}`) {
       setClassName('text-orange-400');
     } else {
       setClassName('hover:text-orange-400');
